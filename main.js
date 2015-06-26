1be3eb0ab9c8de68621b2c8f5a7e47d9b4acabf6
Jamie Binns <jamie.m.binns@gmail.com>
2015-06-26 16:22:46 -0600

    Nearly killed me, but I finished

--- main.js
+++ main.js
@@ -1,56 +1,55 @@
-$(document).on('ready', function(){
+// $(document).on('ready', function(){
 
-	// On click, hide the element and create input (for Name)
-	$('.profile-name').on('click', function(){
-		$('.profile-name').hide();
-		$('.profile-name').after('<input type="text" class="name-input" value="Enter Name">');
-		$('.name-input').focus();
-	});
+// 	// On click, hide the element and create input (for Name)
+// 	$('.profile-name').on('click', function(){
+// 		$('.profile-name').hide();
+// 		$('.profile-name').after('<input type="text" class="name-input" value="Enter Name">');
+// 		$('.name-input').focus();
+// 	});
 
-	// Trigger blur switch (for Name)
-	$('body').on('blur', '.name-input', function(){
-		var $nameInput = $('.name-input').val();
-		$('.profile-name').text($nameInput);
-		$('.name-input').hide();
-		$('.profile-name').show();
-	});	
+// 	// Trigger blur switch (for Name)
+// 	$('body').on('blur', '.name-input', function(){
+// 		var $nameInput = $('.name-input').val();
+// 		$('.profile-name').text($nameInput);
+// 		$('.name-input').hide();
+// 		$('.profile-name').show();
+// 	});	
 
-	// Same for the Bio
-	$('.profile-bio').on('click', function(){
-		$('.profile-bio').hide();
-		$('.profile-bio').after('<input type="text" class="bio-input" value="You have 160 characters for your life story.">');
-		$('.bio-input').focus();
-	});
+// 	// Same for the Bio
+// 	$('.profile-bio').on('click', function(){
+// 		$('.profile-bio').hide();
+// 		$('.profile-bio').after('<input type="text" class="bio-input" value="You have 160 characters for your life story.">');
+// 		$('.bio-input').focus();
+// 	});
 
-	$('body').on('blur', '.bio-input', function(){
-		var $bioInput = $('.bio-input').val();
-		$('.profile-bio').text($bioInput);
-		$('.bio-input').hide();
-		$('.profile-bio').show();
-	});	
-});	
+// 	$('body').on('blur', '.bio-input', function(){
+// 		var $bioInput = $('.bio-input').val();
+// 		$('.profile-bio').text($bioInput);
+// 		$('.bio-input').hide();
+// 		$('.profile-bio').show();
+// 	});	
+// });	
 	
 
 
 // Editable Function
 
-// $(document).on('ready', function(){
+$(document).on('ready', function(){
 
-// 	$('.editable').on('click', function(){
-// 		var $edit = $(this);
-// 		$edit.hide();
-// 		$edit().after('<input class="editable-input" type="text" value="Write here">');
-// 		$('.editable-input').focus();
+		$('.editable').on('click', function(){
+			var $edit = $(this);
+			$edit.hide();
+			$edit.after('<input class="editable-input" type="text" placeholder="Write here">');
+			$edit.focus();
+		});
 
-// 		$('body').on('blur', '.editable-input', function(){
-// 			$('.editable-input').val() === '';
-// 			var $editInput = $('.editable-input').val();
-// 			$edit.text($editInput);
-// 			$('.editable-input').hide();
-// 			$edit.show();
-// 		});
-// 	});
-// });
+		$('body').on('blur', '.editable-input', function(){
+			var $editInput = $(this).val();
+			$(this).prev().text($editInput);
+			$(this).hide();
+			$(this).prev().show();
+		});
+});
 
 
 
